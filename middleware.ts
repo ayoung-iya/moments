06a5c14b973b3aaadebbc8a3467f5f9cd1b9a6b3
ayoung-iya <rhaehfdl1879@naver.com>
2024-12-26 02:37:48 +0900

    feat: 미들웨어를 활용하여 로그인유무와 publicURL 여부에 따라 적절한 url로 redirect

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import {getSession} from "./lib/session";
+
+const PUBLIC_URLS = new Set(["/", "/login", "/create-account"]);
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  const isPublicPath = PUBLIC_URLS.has(request.nextUrl.pathname);
+
+  if (!(session.id || isPublicPath)) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (session.id && isPublicPath) {
+    return NextResponse.redirect(new URL("/products", request.url));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
