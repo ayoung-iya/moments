d33eacdcef66fd57746496fae99e3bab93cf4283
ayoung-iya <rhaehfdl1879@naver.com>
2025-01-17 02:24:01 +0900

    feat: tweetDetails(트윗 정보 묶음)컴포넌트 생성

---
+++ components/tweetDetails.tsx
@@ -0,0 +1,28 @@
+import { Tweets } from "@/app/(tab)/tweet/page";
+import Tweet from "./tweet/tweet";
+
+export default function TweetDetails({
+  id,
+  created_at,
+  tweet,
+  photo,
+  photoWidth,
+  photoHeight,
+  username,
+}: Tweets["tweets"][number]) {
+  return (
+    <>
+      <div className="flex flex-col gap-1.5 py-3">
+        <Tweet.NameWithDate name={username} date={created_at} />
+        <Tweet.Content>{tweet}</Tweet.Content>
+      </div>
+      {photo && <Tweet.Image url={photo} width={photoWidth!} height={photoHeight!} />}
+      <Tweet.ButtonLIst>
+        <Tweet.Button className="hover:bg-stone-100">좋아요</Tweet.Button>
+        <Tweet.Button as="a" href={`/tweet/${id}`} className="hover:bg-stone-100">
+          댓글달기
+        </Tweet.Button>
+      </Tweet.ButtonLIst>
+    </>
+  );
+}
