a81449cf3588054c015582f5677bafe36b36b8d5
ayoung-iya <rhaehfdl1879@naver.com>
2025-01-21 17:55:44 +0900

    feat: tweet 인터셉터 상세 페이지에 댓글 context 적용, commentList 추가

--- app/(tab)/@modal/(...)tweet/[id]/page.tsx
+++ app/(tab)/@modal/(...)tweet/[id]/page.tsx
@@ -1,7 +1,12 @@
-import NewComment from "@/components/commentForm";
+import CommentForm from "@/components/commentForm";
 import TweetDetails from "@/components/tweetDetails";
 import FramedInterceptModal from "@/components/framedInterceptModal";
 import db from "@/lib/db";
+import CommentList from "@/components/commentList";
+import { PromiseReturnType } from "@prisma/client/extension";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import CommentsProvider from "@/context/commentsContext";
+import { getMe } from "@/services/userService";
 
 const fetchTweet = async (id: number) => {
   const tweet = await db.tweet.findUnique({
@@ -30,8 +35,38 @@ const fetchTweet = async (id: number) => {
   return formatTweet;
 };
 
+const getComments = async (tweetId: number) => {
+  "use cache";
+  cacheTag("comments");
+  const comments = await db.comment.findMany({
+    where: { tweetId },
+    include: {
+      user: {
+        select: {
+          username: true,
+        },
+      },
+    },
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+
+  const formatComments = comments.map(({ user, ...comment }) => ({
+    ...comment,
+    username: user.username,
+  }));
+
+  return formatComments;
+};
+
+export type Comments = PromiseReturnType<typeof getComments>;
+
 export default async function ModalTweet({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
+  const myInfo = await getMe();
+  const comments = await getComments(+id);
+
   if (!Number.isInteger(+id)) {
     return null;
   }
@@ -42,17 +77,19 @@ export default async function ModalTweet({ params }: { params: Promise<{ id: str
   }
 
   return (
-    <FramedInterceptModal>
-      <div className="flex h-full flex-col divide-y">
-        <div className="flex h-14 shrink-0 items-center justify-center">
-          <span className="text-lg font-bold">{tweet.username}님의 게시물</span>
+    <CommentsProvider comments={comments}>
+      <FramedInterceptModal>
+        <div className="flex h-full flex-col divide-y">
+          <div className="flex h-14 shrink-0 items-center justify-center">
+            <span className="text-lg font-bold">{tweet.username}님의 게시물</span>
+          </div>
+          <div className="scrollbar-custom grow overflow-y-auto p-3">
+            <TweetDetails {...tweet} />
+            <CommentList initialScrollToTop/>
+          </div>
+          <CommentForm tweetId={+id} username={myInfo!.username} />
         </div>
-        <div className="scrollbar-custom grow overflow-y-auto p-3">
-          <TweetDetails {...tweet} />
-          {/* 댓글리스트 */}
-        </div>
-        <NewComment />
-      </div>
-    </FramedInterceptModal>
+      </FramedInterceptModal>
+    </CommentsProvider>
   );
 }
