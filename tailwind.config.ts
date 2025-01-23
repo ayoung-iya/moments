09de490e8ee60d5c252f9027771e48ac6f19eaf9
ayoung-iya <rhaehfdl1879@naver.com>
2025-01-23 15:09:58 +0900

    chore: tailwind config 추가(box-shadow-top)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      boxShadow: {
+        top: '0 -5px 10px -6px rgba(0,0,0,.1)'
+      },
     },
   },
   plugins: [],
