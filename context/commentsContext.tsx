ca5391c5d50f13d7041aa0e83cf9d1eefa803140
ayoung-iya <rhaehfdl1879@naver.com>
2025-01-23 15:30:11 +0900

    refactor: comment 수정, 삭제 낙관적 업데이트 적용

--- context/commentsContext.tsx
+++ context/commentsContext.tsx
@@ -4,7 +4,7 @@ import { Comments } from "@/app/(tab)/@modal/(...)tweet/[id]/page";
 import { createContext, PropsWithChildren, RefObject, startTransition, useOptimistic, useRef } from "react";
 import { flushSync } from "react-dom";
 
-type Comment = Pick<Comments[number], "id" | "username" | "payload"> &
+export type Comment = Pick<Comments[number], "id" | "username" | "payload"> &
   Partial<Comments[number]> & { isSending?: boolean };
 type CommentsContextType = {
   optimisticComments: Comment[];
@@ -12,11 +12,25 @@ type CommentsContextType = {
 };
 interface CommentsDispatch {
   addComment: (comment: string, username: string) => void;
+  changeComment: (id: number, comment: string) => void;
+  deleteComment: (id: number) => void;
 }
-interface OptimisticAction {
-  newComment: string;
-  username: string;
-}
+type OptimisticAction =
+  | {
+      type: "added";
+      comment: string;
+      username: string;
+    }
+  | {
+      type: "changed";
+      id: number;
+      comment: string;
+    }
+  | {
+      type: "deleted";
+      id: number;
+    };
+
 interface CommentProviderProps extends PropsWithChildren {
   comments: Comments;
 }
@@ -31,31 +45,52 @@ export const CommentsContext = createContext<CommentsContextType>({
 });
 export const CommentsDispatchContext = createContext<CommentsDispatch>({
   addComment: () => {},
+  changeComment: () => {},
+  deleteComment: () => {},
 });
+
+const commentsReducer = (comments: Comment[], action: OptimisticAction) => {
+  switch (action.type) {
+    case "added": {
+      return [
+        { id: randomTemporaryId(), username: action.username, payload: action.comment, isSending: true },
+        ...comments,
+      ];
+    }
+    case "changed": {
+      return comments.map((comment) => (comment.id === action.id ? { ...comment, payload: action.comment } : comment));
+    }
+    case "deleted": {
+      return comments.filter((comment) => comment.id !== action.id);
+    }
+  }
+};
 
 export default function CommentsProvider({ comments, children }: CommentProviderProps) {
   const commentsContainer = useRef<HTMLDivElement>(null);
-  const [optimisticComments, addOptimisticComment] = useOptimistic<Comment[], OptimisticAction>(
+  const [optimisticComments, updateOptimisticComment] = useOptimistic<Comment[], OptimisticAction>(
     comments,
-    (state, { newComment, username }) => [
-      {
-        id: randomTemporaryId(),
-        username: username,
-        payload: newComment,
-        isSending: true,
-      },
-      ...state,
-    ],
+    commentsReducer,
   );
 
   const addComment = (comment: string, username: string) => {
-    flushSync(() => startTransition(() => addOptimisticComment({ newComment: comment, username })));
+    flushSync(() => startTransition(() => updateOptimisticComment({ type: "added", comment, username })));
     commentsContainer.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const changeComment = (id: number, comment: string) => {
+    startTransition(() => updateOptimisticComment({ type: "changed", id, comment }));
+  };
+
+  const deleteComment = (id: number) => {
+    startTransition(() => updateOptimisticComment({ type: "deleted", id }));
   };
 
   return (
     <CommentsContext.Provider value={{ optimisticComments, commentsContainer }}>
-      <CommentsDispatchContext.Provider value={{ addComment }}>{children}</CommentsDispatchContext.Provider>
+      <CommentsDispatchContext.Provider value={{ addComment, changeComment, deleteComment }}>
+        {children}
+      </CommentsDispatchContext.Provider>
     </CommentsContext.Provider>
   );
 }
