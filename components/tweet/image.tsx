952b96f7e8d72de9a0c5763ef86c8b94cf412323
ayoung-iya <rhaehfdl1879@naver.com>
2025-02-01 01:00:33 +0900

    refactor: 이미지 error 발생시 대체 이미지 적용

--- components/tweet/image.tsx
+++ components/tweet/image.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { cloudflareImageVariantURL } from "@/lib/cloudflareImageUtils";
+import { PhotoIcon } from "@heroicons/react/24/solid";
 import NextImage from "next/image";
+import { useState } from "react";
 
 interface ImageProps {
   url: string;
@@ -8,9 +12,27 @@ interface ImageProps {
 }
 
 export default function Image({ url, width, height }: ImageProps) {
+  const [isError, setIsError] = useState(false);
+
   return (
-    <div className="relative -mx-3 w-[100% + 0.75rem * 2]" style={{ aspectRatio: width / height }}>
-      <NextImage className="object-contain object-center" src={cloudflareImageVariantURL(url)} alt="이미지" fill />
+    <div className="w-[100% + 0.75rem * 2] relative -mx-3" style={{ aspectRatio: width / height }}>
+      {!isError && (
+        <NextImage
+          className="object-contain object-center"
+          src={cloudflareImageVariantURL(url)}
+          alt="이미지"
+          fill
+          onError={() => {
+            setIsError(true);
+          }}
+        />
+      )}
+      {isError && (
+        <div className="absolute inset-0 flex flex-col items-center justify-center *:text-stone-700">
+          <PhotoIcon className="size-1/6" />
+          <p>이미지를 불러올 수 없습니다</p>
+        </div>
+      )}
     </div>
   );
 }
