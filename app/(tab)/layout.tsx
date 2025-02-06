3d515126fb0c9f381c74832e1a02362b84320501
ayoung-iya <rhaehfdl1879@naver.com>
2025-02-07 02:10:44 +0900

    feat: footer 추가

--- app/(tab)/layout.tsx
+++ app/(tab)/layout.tsx
@@ -1,4 +1,5 @@
 import Logo from "@/components/logo";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function TabLayout({ children, modal }: { children: React.ReactNode; modal: React.ReactNode }) {
@@ -12,6 +13,14 @@ export default function TabLayout({ children, modal }: { children: React.ReactNo
         <Link href="/profile">MY</Link>
       </nav>
       <main className="mx-auto max-w-screen-sm px-4">{children}</main>
+      <footer className="mt-20 flex justify-between border-t py-10 text-stone-700">
+        <div className="select-none italic">
+          당신의 이야기를 기록하는 공간, <b>Moments.</b>
+        </div>
+        <a href="https://github.com/ayoung-iya/moments" target="_blank">
+          <Image src="/github-mark.svg" width={20} height={20} alt="moment github" />
+        </a>
+      </footer>
     </>
   );
 }
