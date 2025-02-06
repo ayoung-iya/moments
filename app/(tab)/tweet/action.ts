f3be2205f2862fe5747ed831e77d65262d3f9f58
ayoung-iya <rhaehfdl1879@naver.com>
2025-02-07 01:12:42 +0900

    refactor: getTweets page 0부터 시작, 좋아요 여부, 좋아요, 댓글 개수 데이터 추가

--- app/(tab)/tweet/action.ts
+++ app/(tab)/tweet/action.ts
@@ -1,13 +1,21 @@
 "use server";
 import db from "@/lib/db";
+import { PromiseReturnType } from "@prisma/client/extension";
 import { cacheTag } from "next/dist/server/use-cache/cache-tag";
 
-export const getTweets = async (page = 1, size = 5) => {
+interface GetTweetsProps {
+  page?: number;
+  size?: number;
+  currentUserId: number;
+}
+
+export const getTweets = async ({ page = 0, size = 5, currentUserId }: GetTweetsProps) => {
   "use cache";
   cacheTag("tweets");
+
   const totalPages = Math.ceil((await db.tweet.count()) / size);
   const tweets = await db.tweet.findMany({
-    skip: (page - 1) * size,
+    skip: page * size,
     take: size,
     orderBy: {
       created_at: "desc",
@@ -18,12 +26,30 @@ export const getTweets = async (page = 1, size = 5) => {
           username: true,
         },
       },
+      likes: {
+        where: { userId: currentUserId },
+        select: { userId: true },
+      },
+      _count: {
+        select: {
+          likes: true,
+          comments: true,
+        },
+      },
     },
   });
 
-  const formatTweets = tweets.map(({ user, ...tweet }) => {
-    return { ...tweet, username: user.username };
+  const formatTweets = tweets.map(({ user, likes, _count, ...tweet }) => {
+    return {
+      ...tweet,
+      username: user.username,
+      isLiked: !!likes.length,
+      likeCount: _count.likes,
+      commentsCount: _count.comments,
+    };
   });
 
   return { tweets: formatTweets, currentPage: page, totalPages, hasNextPage: page < totalPages };
 };
+
+export type Tweets = PromiseReturnType<typeof getTweets>;
