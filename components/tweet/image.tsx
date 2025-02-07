c914219f549713c157eecd59460fefe0fa2df3f2
ayoung-iya <rhaehfdl1879@naver.com>
2025-02-08 02:36:24 +0900

    refactor: 이미지 크기에 따라 css 수정

--- components/tweet/image.tsx
+++ components/tweet/image.tsx
@@ -15,22 +15,27 @@ export default function Image({ url, width, height }: ImageProps) {
   const [isError, setIsError] = useState(false);
 
   return (
-    <div className="w-[100% + 0.75rem * 2] relative -mx-3" style={{ aspectRatio: width / height }}>
+    <div className="w-[100% + 0.75rem * 2] relative -mx-3 flex items-center justify-center">
       {!isError && (
-        <NextImage
-          className="object-contain object-center"
-          src={cloudflareImageVariantURL(url)}
-          alt="이미지"
-          fill
-          onError={() => {
-            setIsError(true);
-          }}
-        />
+        <div className="relative w-full" style={{ aspectRatio: width / height, maxWidth: `${width}px` }}>
+          <NextImage
+            className="object-contain object-center"
+            style={{ maxWidth: `${width}px` }}
+            src={cloudflareImageVariantURL(url)}
+            alt="이미지"
+            fill
+            onError={() => {
+              setIsError(true);
+            }}
+          />
+        </div>
       )}
       {isError && (
-        <div className="absolute inset-0 flex flex-col items-center justify-center *:text-stone-700">
-          <PhotoIcon className="size-1/6" />
-          <p>이미지를 불러올 수 없습니다</p>
+        <div className="relative h-44 w-full">
+          <div className="absolute inset-0 flex flex-col items-center justify-center *:text-stone-700">
+            <PhotoIcon className="size-1/2" />
+            <p>이미지를 불러올 수 없습니다</p>
+          </div>
         </div>
       )}
     </div>
