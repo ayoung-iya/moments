5a8350920127bced018534d487c10c206d1eca26
ayoung-iya <rhaehfdl1879@naver.com>
2025-02-07 16:03:42 +0900

    style: nav 태그 header 태그로 변경, footer css 변경

--- app/(tab)/layout.tsx
+++ app/(tab)/layout.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 export default function TabLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <nav className="flex items-center justify-between px-4 py-3">
+      <header className="flex items-center justify-between px-4 py-3">
         <Link href="/">
           <Logo size="sm" />
         </Link>
         <Link href="/profile">MY</Link>
-      </nav>
+      </header>
       <main className="mx-auto max-w-screen-sm px-4">{children}</main>
-      <footer className="mt-20 flex justify-between border-t py-10 text-stone-700">
+      <footer className="mt-20 flex justify-between border-t px-4 py-10 text-stone-700">
         <div className="select-none italic">
           당신의 이야기를 기록하는 공간, <b>Moments.</b>
         </div>
