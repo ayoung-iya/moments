2491b3bbc3912ca2966c14939d138427544edb1d
ayoung-iya <rhaehfdl1879@naver.com>
2025-02-08 00:06:24 +0900

    feat: /tweet/[id], /tweet/[id]/edit 페이지 생성

--- app/(tab)/tweet/[id]/edit/page.tsx
+++ app/(tab)/tweet/[id]/edit/page.tsx
@@ -1,3 +1,25 @@
-export default function TweetEditPage() {
-  return <></>;
+import { getTweet } from "@/services/tweetService";
+import { notFound, redirect } from "next/navigation";
+import TweetEditForm from "@/components/tweetEditForm";
+import { getSession } from "@/lib/session";
+
+export default async function TweetEditPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const { id: userId } = await getSession();
+  const tweet = await getTweet(+id);
+
+  if (!tweet) {
+    notFound();
+  }
+
+  if (userId !== tweet.userId) {
+    redirect("/tweet");
+  }
+
+  return (
+    <div className="mt-5 flex w-full flex-col items-center gap-5">
+      <h2 className="text-xl font-bold">게시물 수정하기</h2>
+      <TweetEditForm {...tweet} />
+    </div>
+  );
 }
--- app/(tab)/tweet/[id]/page.tsx
+++ app/(tab)/tweet/[id]/page.tsx
@@ -1,5 +1,9 @@
+import CommentCreateForm from "@/components/commentCreateForm";
+import CommentList from "@/components/commentList";
 import TweetDetails from "@/components/tweetDetails";
+import CommentsProvider from "@/context/commentsContext";
 import db from "@/lib/db";
+import { getComments } from "@/services/commentService";
 import { getIsLike } from "@/services/likeService";
 import { getMe } from "@/services/userService";
 import { notFound } from "next/navigation";
@@ -39,6 +43,19 @@ export default async function Tweet({ params }: { params: Promise<{ id: string }
   }
   const tweet = await fetchTweet(+id);
   const likeData = await getIsLike(+id, myInfo!.id);
+  const commentsData = await getComments(+id);
 
-  return <TweetDetails {...tweet} currentUserId={myInfo!.id} {...likeData} />;
+  return (
+    <CommentsProvider {...commentsData}>
+      <div className="rounded-md border bg-white bg-opacity-100 px-3">
+        <TweetDetails {...tweet} currentUserId={myInfo!.id} {...likeData} />
+        <div className="flex flex-col gap-3 border-t py-3">
+          <div className="rounded-3xl bg-stone-50">
+            <CommentCreateForm tweetId={+id} username={myInfo!.username} />
+          </div>
+          {!!commentsData.commentsCount && <CommentList />}
+        </div>
+      </div>
+    </CommentsProvider>
+  );
 }
