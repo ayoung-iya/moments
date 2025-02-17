024f24afb2415f19058ae62bcd8fffb6d10cbadd
ayoung-iya <rhaehfdl1879@naver.com>
2025-02-17 20:26:05 +0900

    refactor: layout 분리

--- app/(tab)/(default)/layout.tsx
+++ app/(tab)/(default)/layout.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
-  return <div className="mx-auto w-full max-w-screen-sm pb-12 sm:pb-16">{children}</div>;
+  return (
+    <>
+      <main className="mx-auto w-full max-w-screen-sm flex-1 pb-12 sm:pb-16">{children}</main>
+      <footer className="mt-3 flex items-center justify-between py-7 text-stone-700 shadow-t-border sm:py-10">
+        <div className="select-none text-xs italic">
+          당신의 이야기를 기록하는 공간, <br />
+          <b>Moments.</b>
+        </div>
+        <a href="https://github.com/ayoung-iya/moments" target="_blank">
+          <Image src="/github-mark.svg" width={20} height={20} alt="moment github" />
+        </a>
+      </footer>
+    </>
+  );
 }
--- app/(tab)/layout.tsx
+++ app/(tab)/layout.tsx
@@ -1,26 +1,16 @@
 import Logo from "@/components/logo";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function TabLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col">
-      <header className="flex items-center justify-between px-4 py-3">
+    <div className="flex min-h-screen flex-col px-4">
+      <header className="flex items-center justify-between py-3">
         <Link href="/">
           <Logo size="sm" />
         </Link>
         <Link href="/profile">MY</Link>
       </header>
-      <main className="mx-auto w-full max-w-screen-sm grow px-4 pb-12 sm:pb-16">{children}</main>
-      <footer className="shadow-t-border flex items-center justify-between px-4 py-7 text-stone-700 sm:py-10">
-        <div className="select-none text-xs italic">
-          당신의 이야기를 기록하는 공간, <br />
-          <b>Moments.</b>
-        </div>
-        <a href="https://github.com/ayoung-iya/moments" target="_blank">
-          <Image src="/github-mark.svg" width={20} height={20} alt="moment github" />
-        </a>
-      </footer>
+      <div className="flex h-full w-full grow flex-col">{children}</div>
     </div>
   );
 }
